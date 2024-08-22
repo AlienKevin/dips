776ffa19218f99ceaf4b246671979a8d9e4a8b5e
Kevin Li <kevinli020508@gmail.com>
2024-08-22 12:15:32 +0800

    Update hanlp_gen_seg_dataset.py to run in parallel

--- hanlp_gen_seg_dataset.py
+++ hanlp_gen_seg_dataset.py
@@ -3,35 +3,39 @@
 import hanlp
 from datasets import load_dataset, DatasetDict
 import torch
+from multiprocess import set_start_method
 
-device = "cuda" if torch.cuda.is_available() else ("mps" if torch.backends.mps.is_available() else "cpu")
-tok_fine = hanlp.load(hanlp.pretrained.tok.FINE_ELECTRA_SMALL_ZH, devices=[device])
-
-dataset = load_dataset("beyond/chinese_clean_passages_80m", split="train")
-
-# Define a function to tokenize sentences
-def tokenize_sentence(example):
-    tokens = tok_fine(example["passage"])
-    return {"tokens": tokens}
-
-# Apply the tokenization function
-dataset = dataset.map(tokenize_sentence, batched=True, remove_columns=["passage"], batch_size=10000)
-
-# Split the dataset into train, validation, and test sets
-train_testvalid = dataset.train_test_split(test_size=0.2, seed=42)
-test_valid = train_testvalid['test'].train_test_split(test_size=0.5, seed=42)
-
-dataset = DatasetDict({
-    'train': train_testvalid['train'],
-    'validation': test_valid['train'],
-    'test': test_valid['test']
-})
-
-print(f"Train set: {len(dataset['train'])} samples")
-print(f"Validation set: {len(dataset['validation'])} samples")
-print(f"Test set: {len(dataset['test'])} samples")
-
-# Upload the dataset to the Hugging Face Hub
-dataset.push_to_hub("AlienKevin/genius-seg")
-
-print(f"Uploaded {len(dataset)} segmented sentences to AlienKevin/genius-seg on the Hugging Face Hub")
+if __name__ == '__main__':
+    set_start_method('spawn')
+    torch.multiprocessing.set_start_method('spawn')
+    
+    dataset = load_dataset("beyond/chinese_clean_passages_80m", split="train")
+    
+    # Define a function to tokenize sentences
+    def tokenize_sentence(example):
+        device = "cuda" if torch.cuda.is_available() else ("mps" if torch.backends.mps.is_available() else "cpu")
+        tok_fine = hanlp.load(hanlp.pretrained.tok.FINE_ELECTRA_SMALL_ZH, devices=[device])
+        tokens = tok_fine(example["passage"])
+        return {"tokens": tokens}
+    
+    # Apply the tokenization function
+    dataset = dataset.select(range(10000000)).map(tokenize_sentence, batched=True, remove_columns=["passage"], batch_size=10000, num_proc=10)
+    
+    # Split the dataset into train, validation, and test sets
+    train_testvalid = dataset.train_test_split(test_size=0.2, seed=42)
+    test_valid = train_testvalid['test'].train_test_split(test_size=0.5, seed=42)
+    
+    dataset = DatasetDict({
+        'train': train_testvalid['train'],
+        'validation': test_valid['train'],
+        'test': test_valid['test']
+    })
+    
+    print(f"Train set: {len(dataset['train'])} samples")
+    print(f"Validation set: {len(dataset['validation'])} samples")
+    print(f"Test set: {len(dataset['test'])} samples")
+    
+    # Upload the dataset to the Hugging Face Hub
+    dataset.push_to_hub("AlienKevin/genius-seg")
+    
+    print(f"Uploaded segmented sentences to AlienKevin/genius-seg on the Hugging Face Hub")
