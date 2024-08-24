efae9f4aeebfdc748ac566657ad7c1b865af15d5
Kevin Li <kevinli020508@gmail.com>
2024-08-24 17:33:06 +0800

    Added BERT as an alternative architecture to segmenter

--- segmenter.py
+++ segmenter.py
@@ -37,43 +37,129 @@ def load_bpe_mappings(file_path):
 bpe_mappings = load_bpe_mappings('data/Cangjie5_SC_BPE.txt')
 
 
-class ConvMLM(nn.Module):
-    def __init__(self, cangjie_expand, vocab, embedding_dim=200, hidden_dims=[200, 200, 200, 200, 200], num_layers=5, tagset=None, char_embedding_path=None):
-        super(ConvMLM, self).__init__()
+class BertConfig:
+    def __init__(
+        self,
+        hidden_size: int = 768,
+        num_hidden_layers: int = 12,
+        num_attention_heads: int = 12,
+        intermediate_size: int = 3072,
+        hidden_act: str = "gelu",
+        hidden_dropout_prob: float = 0.1,
+        attention_probs_dropout_prob: float = 0.1,
+        max_position_embeddings: int = 200,
+        initializer_range: float = 0.0,
+        **kwargs,  # unused
+    ):
+        self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.hidden_act = hidden_act
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.hidden_size = hidden_size
+        self.initializer_range = initializer_range
+        self.intermediate_size = intermediate_size
+        self.max_position_embeddings = max_position_embeddings
+        self.num_attention_heads = num_attention_heads
+        self.num_hidden_layers = num_hidden_layers
+
+    @staticmethod
+    def from_json(path: str) -> "BertConfig":
+        with open(path, "r") as f:
+            file_content = json.load(f)
+
+        return BertConfig(**file_content)
+
+
+class ConvConfig:
+    def __init__(self, embedding_size=200, hidden_sizes=[200, 200, 200, 200, 200], kernel_size=3):
+        self.embedding_size = embedding_size
+        self.hidden_sizes = hidden_sizes
+        self.kernel_size = kernel_size
+    
+    @staticmethod
+    def from_json(path: str) -> "ConvConfig":
+        with open(path, "r") as f:
+            file_content = json.load(f)
+
+        return ConvConfig(**file_content)
+
+
+class Segmenter(nn.Module):
+    def __init__(self, cangjie_expand, vocab, config=ConvConfig(), tagset=None, char_embedding_path=None):
+        super(Segmenter, self).__init__()
         self.vocab = vocab
+
+        self.config = config
+
+        if isinstance(config, ConvConfig):
+            embedding_size = config.embedding_size
+            output_size = config.hidden_sizes[-1]
+        elif isinstance(config, BertConfig):
+            embedding_size = config.hidden_size
+            output_size = config.hidden_size
+        
         if char_embedding_path is None:
-            self.embedding = nn.Embedding(len(vocab), embedding_dim)
+            self.embedding = nn.Embedding(len(vocab), embedding_size)
         else:
             self.embedding = read_pretrained_embeddings(char_embedding_path, vocab, freeze=False)
             print('Loaded embeddings')
         self.tagset = tagset
         self.cangjie_expand = cangjie_expand
 
-        # Dilated convolutions
-        self.conv_layers = nn.ModuleList([
-            nn.Conv1d(
-                in_channels=embedding_dim if i == 0 else hidden_dims[i-1],
-                out_channels=hidden_dims[i],
-                kernel_size=3,
-                dilation=2**i if self.cangjie_expand else 1,
-                padding='same'
-            ) for i in range(num_layers)
-        ])
-        
-        self.relu = nn.ReLU()
-        self.output = nn.Linear(hidden_dims[-1], len(vocab) if not tagset else len(tagset))
+        if isinstance(config, ConvConfig):
+            self.conv_layers = nn.ModuleList([
+                nn.Conv1d(
+                    in_channels=embedding_size if i == 0 else config.hidden_sizes[i-1],
+                    out_channels=config.hidden_sizes[i],
+                    kernel_size=config.kernel_size,
+                    dilation=2**i if self.cangjie_expand else 1,
+                    padding='same'
+                ) for i in range(len(config.hidden_sizes))
+            ])
+            self.relu = nn.ReLU()
+            self.output = nn.Linear(output_size, len(vocab) if not tagset else len(tagset))
+        elif isinstance(config, BertConfig):
+            self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+
+            self.embedding_layer_norm = nn.LayerNorm(config.hidden_size)
+            self.embedding_dropout = nn.Dropout(p=config.hidden_dropout_prob)
+
+            self.encoders = nn.TransformerEncoder(
+                encoder_layer=nn.TransformerEncoderLayer(
+                    d_model=config.hidden_size,
+                    nhead=config.num_attention_heads,
+                    dim_feedforward=config.intermediate_size,
+                    dropout=config.hidden_dropout_prob,
+                    activation=config.hidden_act,
+                    batch_first=True,
+                ),
+                num_layers=config.num_hidden_layers,
+            )
+            self.output = nn.Sequential(
+                nn.Linear(config.hidden_size, config.hidden_size),
+                nn.LayerNorm(config.hidden_size),
+                nn.GELU(),
+                nn.Linear(config.hidden_size, len(vocab) if not tagset else len(tagset), bias=True)
+            )
 
     def forward(self, x):
         # x shape: (batch_size, sequence_length)
-        x = self.embedding(x).transpose(1, 2)
-        # x shape: (batch_size, embedding_dim, sequence_length)
+        x = self.embedding(x)
 
-        for conv in self.conv_layers:
-            x = self.relu(conv(x))
-
-        # x shape: (batch_size, hidden_dim, sequence_length)
-        x = x.transpose(1, 2)
-        # x shape: (batch_size, sequence_length, hidden_dim)
+        if isinstance(self.config, ConvConfig):
+            x = x.transpose(1, 2)
+            # x shape: (batch_size, embedding_dim, sequence_length)
+            for conv in self.conv_layers:
+                x = self.relu(conv(x))
+            # x shape: (batch_size, hidden_dim, sequence_length)
+            x = x.transpose(1, 2)
+            # x shape: (batch_size, sequence_length, hidden_dim)
+        elif isinstance(self.config, BertConfig):
+            seq_length = x.size(1)
+            position_ids = torch.arange(seq_length, dtype=torch.long, device=x.device)
+            x = x + self.position_embeddings(position_ids).unsqueeze(0)
+            x = self.embedding_layer_norm(x)
+            x = self.embedding_dropout(x)
+            x = self.encoders(x)
 
         return self.output(x)
     
@@ -83,22 +169,25 @@ class ConvMLM(nn.Module):
                 'state_dict': self.state_dict(),
                 'vocab': self.vocab.token2id_map,
                 'tagset': self.tagset,
+                'config': self.config,
                 'cangjie_expand': self.cangjie_expand
             }, path)
         else:
             torch.save({
                 'state_dict': self.state_dict(),
                 'vocab': self.vocab.token2id_map,
+                'config': self.config,
                 'cangjie_expand': self.cangjie_expand
             }, path)
 
     @classmethod
     def load(cls, path):
         checkpoint = torch.load(path, weights_only=False)
+        config = checkpoint.get('config')
         vocab = Vocab(checkpoint['vocab'])
         tagset = checkpoint.get('tagset')
         cangjie_expand = checkpoint.get('cangjie_expand', False)
-        model = cls(cangjie_expand, vocab, tagset=tagset)
+        model = cls(cangjie_expand, vocab, config=config, tagset=tagset)
         model.load_state_dict(checkpoint['state_dict'])
         return model
     
@@ -119,18 +208,44 @@ class ConvMLM(nn.Module):
         input_ids = [self.vocab[char] for char in mapped_text]
         
         # Step 3: Run the model and get top tag predictions
-        with torch.no_grad():
-            input_tensor = torch.tensor(input_ids).unsqueeze(0)  # Add batch dimension
-            output = self(input_tensor)
-            predictions = torch.argmax(output, dim=-1).squeeze(0)
-
-        # Step 4: Map predicted tags back to the original text
         tagged_text = []
-        for i, pred in enumerate(predictions):
-            if i in token_map:
-                char = text[token_map[i]]
-                tag = self.tagset.id2token(pred.item())
-                tagged_text.append((char, tag))
+        if isinstance(self.config, BertConfig):
+            # Windowed inference to overcome context size limit of BERT
+            max_length = self.config.max_position_embeddings
+            chunk_size = max_length
+            overlap = 10
+            
+            for start in range(0, len(input_ids), chunk_size - overlap):
+                end = min(start + chunk_size, len(input_ids))
+                chunk_input_ids = input_ids[start:end]
+                
+                with torch.no_grad():
+                    input_tensor = torch.tensor(chunk_input_ids).unsqueeze(0)  # Add batch dimension
+                    output = self(input_tensor)
+                    predictions = torch.argmax(output, dim=-1).squeeze(0)
+                
+                # Map predicted tags back to the original text for this chunk
+                for i, pred in enumerate(predictions):
+                    global_index = start + i
+                    if global_index in token_map and (start == 0 or i >= overlap):
+                        char = text[token_map[global_index]]
+                        tag = self.tagset.id2token(pred.item())
+                        tagged_text.append((char, tag))
+                
+                if end == len(input_ids):
+                    break
+        else:
+            with torch.no_grad():
+                input_tensor = torch.tensor(input_ids).unsqueeze(0)  # Add batch dimension
+                output = self(input_tensor)
+                predictions = torch.argmax(output, dim=-1).squeeze(0)
+
+            # Map predicted tags back to the original text
+            for i, pred in enumerate(predictions):
+                if i in token_map:
+                    char = text[token_map[i]]
+                    tag = self.tagset.id2token(pred.item())
+                    tagged_text.append((char, tag))
 
         return tagged_text
 
@@ -251,7 +366,7 @@ def train(model, model_path, train_dataloader, validation_dataset, validation_da
     best_val_loss = float('inf')
     global_step = 0
 
-    wandb.init(project="conv-mlm", name=os.path.basename(model_path))
+    wandb.init(project="conv-mlm", name=os.path.splitext(os.path.basename(model_path))[0])
 
     for epoch in range(num_epochs):
         for batch, (input_ids, labels) in tqdm(enumerate(train_dataloader), total=len(train_dataloader)):
@@ -348,14 +463,14 @@ def load_train_dataset(args):
 
         train_dataset = TaggerDataset(train_dataset, window_size=-1, tag_context_size=-1, vocab_threshold=args.vocab_threshold, sliding=False)
         train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size,
-                                      collate_fn=lambda batch: pad_batch_seq(batch))
+                                      collate_fn=lambda batch: pad_batch_seq(batch, max_sequence_length=args.max_sequence_length))
 
         print('Training dataset vocab size:', len(train_dataset.vocab))
         print('Training dataset tagset size:', len(train_dataset.tagset))
 
         validation_dataset = TaggerDataset(validation_dataset, window_size=-1, tag_context_size=-1, vocab_threshold=args.vocab_threshold, vocab=train_dataset.vocab, tagset=train_dataset.tagset, sliding=False)
         validation_dataloader = DataLoader(validation_dataset, batch_size=args.batch_size,
-                                           collate_fn=lambda batch: pad_batch_seq(batch))
+                                           collate_fn=lambda batch: pad_batch_seq(batch, max_sequence_length=args.max_sequence_length))
     
         # Reload validation in the same format as the test dataset for calculating F1 score
         validation_dataset = load_tagged_dataset(dataset_name, split='validation', tagging_scheme=args.tagging_scheme, output_format='test')
@@ -367,7 +482,7 @@ def train_model(args, model_path, device):
     train_dataset, train_dataloader, validation_dataset, validation_dataloader = load_train_dataset(args)
 
     tagset = train_dataset.tagset if hasattr(train_dataset, 'tagset') else None
-    model = ConvMLM(args.cangjie_expand, train_dataset.vocab, tagset=tagset, char_embedding_path=args.char_embedding_path)
+    model = Segmenter(args.cangjie_expand, train_dataset.vocab, config=args.config, tagset=tagset, char_embedding_path=args.char_embedding_path)
     model.to(device)
 
     # Training setup
@@ -387,7 +502,7 @@ def train_model(args, model_path, device):
 def test_model(args, model_path):
     test_dataset = load_tagged_dataset(args.test_dataset, 'test')
     
-    model = ConvMLM.load(model_path)
+    model = Segmenter.load(model_path)
 
     results, errors = score_tags(test_dataset, lambda text: model.tag(text))
 
@@ -415,7 +530,7 @@ def test_model(args, model_path):
 
 
 def infer_model(args, model_path):
-    model = ConvMLM.load(model_path)
+    model = Segmenter.load(model_path)
 
     for text in open(args.test_file, 'r', encoding='utf-8').readlines():
         tagged = merge_tokens(model.tag(text))
@@ -430,15 +545,16 @@ def main():
 
     import argparse
 
-    parser = argparse.ArgumentParser(description='Train ConvMLM model on selected dataset')
+    parser = argparse.ArgumentParser(description='Train segmenter model on selected dataset')
     parser.add_argument('--mode', type=str, choices=['train', 'infer', 'test'], required=True, help='Mode to run in')
+    parser.add_argument('--config', type=str, choices=['conv', 'bert'], default='conv', help='Architecture to use')
     parser.add_argument('--train_dataset', type=str, choices=['rthk', 'genius', 'tte', 'cityu-seg', 'as-seg', 'msr-seg', 'pku-seg', 'genius-seg'],
                         help='Dataset to use for training')
     parser.add_argument('--test_dataset', type=str, choices=['as-seg', 'cityu-seg', 'msr-seg', 'pku-seg', 'genius-seg', 'ctb8'],
                         help='Dataset to use for testing')
     parser.add_argument('--test_file', type=str, help='File to use for testing')
     parser.add_argument('--batch_size', type=int, default=100, help='Batch size for training')
-    parser.add_argument('--num_epochs', type=int, default=40, help='Number of epochs to train for')
+    parser.add_argument('--num_epochs', type=int, default=5, help='Number of epochs to train for')
     parser.add_argument('--validation_steps', type=float, default=0.2, help='Validation steps')
     parser.add_argument('--max_sequence_length', type=int, default=200, help='Maximum sequence length')
     parser.add_argument('--segmentation', action='store_true', help='Do segmentation rather than MLM')
@@ -448,7 +564,9 @@ def main():
     parser.add_argument('--char_embedding_path', type=str, help='Path to character embeddings')
     args = parser.parse_args()
 
-    model_path = f'models/conv_{args.train_dataset}{'_mlm' if not args.segmentation else ''}.pth'
+    model_path = f'models/{args.config}_{args.train_dataset}{'_mlm' if not args.segmentation else ''}.pth'
+
+    args.config = ConvConfig() if args.config == 'conv' else BertConfig()
 
     if args.mode == 'train':
         train_model(args, model_path, device)
